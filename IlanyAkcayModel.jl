3777a043949eb0f763636ca0f99ea93bad8ff753
parkercsmith <46355904+parkercsmith@users.noreply.github.com>
2019-03-29 15:03:21 -0400

    Add files via upload

--- IlanyAkcayModel.jl
+++ IlanyAkcayModel.jl
@@ -21,6 +21,12 @@ mutable struct globalstuff
     popSize::Int64 #number of individuals in population
     edgeMatrix::Array{Int64, 2} #matrix of binary Ints; 1 where connection between row and col index is present
     meanCoopRatio::Float64 #summed over 500 generations, then calculated after core function
+    meanProbNeighbor::Float64
+    meanProbRandom::Float64
+    meanDegree::Float64
+    meanCoopDegree::Float64
+    meanDefDegree::Float64
+    meanCoopDefDistance::Float64
     population::Array{Node, 1} #contains 100 Node structs
     numInitialCoops::Int64 #number of cooperators at initialization of globalstuff; used once per run
     numInitEdges::Int64 #number of edges in network at initialization of globalstuff; used once per run
@@ -40,7 +46,13 @@ mutable struct globalstuff
     function globalstuff(ben::Float64, cL::Float64)
         #basic variables for measurement
         popSize = 100
+        meanProbNeighbor = 0.0
+        meanProbRandom = 0.0
+        meanDegree = 0.0
         meanCoopRatio = 0.0
+        meanCoopDegree = 0.0
+        meanDefDegree = 0.0
+        meanCoopDefDistance = 0.0
 
         #initialization factors (may need to be adjusted)
         numInitialCoops = popSize/2
@@ -96,7 +108,88 @@ function countCoops(over::globalstuff)
         pNTotal += over.population[i].pN
     end
     pNTotal /= over.popSize
-    over.meanCoopRatio += pNTotal
+    over.meanProbNeighbor += pNTotal
+
+    pRTotal = 0.0
+    for(i) in 1:over.popSize
+        pRTotal += over.population[i].pR
+    end
+    pRTotal /= over.popSize
+    over.meanProbRandom += pRTotal
+
+    coopTotal = 0.0
+    for(i) in 1:over.popSize
+        if(over.population[i].strategy == 1)
+            coopTotal += 1.0
+        end
+    end
+    coopTotal /= over.popSize
+    over.meanCoopRatio += coopTotal
+
+    degTotal = 0.0
+    coopDegTotal = 0.0
+    defDegTotal = 0.0
+    for(i) in 1:over.popSize
+        degCounter = 0
+        for(ii) in 1:over.popSize
+            if(over.edgeMatrix[i][ii] == 1)
+                degCounter += 1
+            end
+        end
+        degTotal += degCounter
+        if(over.population[i].strategy == 1)
+            coopDegTotal += degCounter
+        else
+            defDegTotal += degCounter
+        end
+    end
+    degTotal /= over.popSize
+    defDegTotal /= over.popSize
+    coopDegTotal /= over.popSize
+    over.meanDegree += degTotal
+    over.meanCoopDegree += coopDegTotal
+    over.meanDefDegree += defDegTotal
+
+    distanceTotal = 0.0
+    for(i) in 1:over.popSize
+        found = false
+        usualSuspects = zeros(over.popSize * over.popSize)
+        susCounter = 1
+        distCount = 1
+        for(ii) in 1:over.popSize
+            if(over.edgeMatrix[i][ii] == 1)
+                usualSuspects[susCounter] = ii
+                susCounter += 1
+            end
+        end
+        while(!found)
+            dC = distCount
+            if(dC == 1)
+                for(s) in 1:length(usualSuspects)
+                    if(usualSuspects[s] != 0)
+                        if(over.population[usualSuspects[s]].strategy != over.population[i].strategy)
+                            found = true
+                        end
+                    end
+                end
+            else
+                for(s) in 1:length(usualSuspects)
+                    if(usualSuspects[s] != 0)
+                        for(ii) in 1:over.popSize
+                            if(over.edgeMatrix[usualSuspects[s]][ii] == 1)
+                                usualSuspects[susCounter] = ii
+                                susCounter += 1
+                            end
+                        end
+                    end
+                end
+                dC -= 1
+            end
+        end
+        distanceTotal += distCount
+    end
+    distanceTotal /= over.popSize
+    over.meanCoopDefDistance += distanceTotal
 
     #vestigial bar chart formatting (may be used later)
     #=
@@ -262,11 +355,12 @@ end
 parsedArgs = parse_args(ARGS, argTab)
 costLink = parsedArgs["cLink"]
 
-for(b) in 1:1 #edit here
-    benWeights = zeros(1)
+for(b) in 0:1:10 #edit here
+    #=benWeights = zeros(1)
     benWeights[b] = 1
     benWeights = weights(Array{Float64, 1}(benWeights))
-    currBenefit = sample([3.0] , benWeights)
+    currBenefit = sample([3.0] , benWeights)=#
+    currBenefit = b
     #replicates data for 100 simulations
     finalMeanPN = 0.0
     benVal = 0.0
@@ -281,18 +375,37 @@ for(b) in 1:1 #edit here
         runGens(overlord)
 
         #divides meanCooperationRatio by last 400 generations to get a true mean, then outputs
+        overlord.meanProbNeighbor = overlord.meanProbNeighbor/80000.0
+        overlord.meanProbRandom = overlord.meanProbRandom/80000.0
+        overlord.meanDegree = overlord.meanDegree/80000.0
+        overlord.meanCoopDegree = overlord.meanCoopDegree/80000.0
+        overlord.meanDefDegree = overlord.meanDefDegree/80000.0
         overlord.meanCoopRatio = overlord.meanCoopRatio/80000.0
+        overlord.meanCoopDefDistance = overlord.meanCoopDefDistance/80000.0
+
         if(x==1)
         #    println("Simulation at popSize = $(overlord.popSize) and cLink = $(overlord.cLink)")
         end
-        finalMeanPN += overlord.meanCoopRatio
+        finalMeanPN += overlord.meanProbNeighbor
+        finalMeanPR += overlord.meanProbRandom
+        finalMeanDegree += overlord.meanDegree
+        finalMeanCoopDegree += overlord.meanCoopDegree
+        finalMeanDefDegree += overlord.meanDefDegree
+        finalMeanDistance += overlord.meanCoopDefDistance
+        finalMeanCoopRatio += overlord.meanCoopRatio
     end
     finalMeanPN /= 10.0
+    finalMeanPR /= 10.0
+    finalMeanDegree /= 10.0
+    finalMeanCoopDegree /= 10.0
+    finalMeanDefDegree /= 10.0
+    finalMeanDistance /= 10.0
+    finalMeanCoopRatio /= 10.0
     #=
     popSizeStr = "$(currPopSize)"
     while(length(popSizeStr)<4)
         popSizeStr = "0" * popSizeStr
     end
     =#
-    save("highpnData_$(costLink)_B$(benVal).jld2", "parameters", [costLink, benVal], "meanPN", finalMeanPN)
+    save("exploratoryData_$(costLink)_B$(benVal).jld2", "parameters", [costLink, benVal], "meanPN", finalMeanPN, "meanPR", finalMeanPR, "meanDegree", finalMeanDegree, "meanDefectorDegree", finalMeanDefDegree, "meanCooperatorDegree", finalMeanCoopDegree, "meanDistanceFromDefToCoop", finalMeanDistance, "meanCooperationRatio", finalMeanCoopRatio)
 end
